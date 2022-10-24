8c9a2c1d5f36d5f46721aa1d68775f1f23e20d55
Kirill Shaplyko <k.shaplyko@gmail.com>
2022-10-24 11:42:20 +0200

    fix(deps): remove postinsall to use from github directly

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,10 +2,16 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import * as QUnit from 'qunit';
+import * as td from 'testdouble';
 
-import './assertions/dom';
-import './assertions/verify';
-import './assertions/wait-for';
+import installVerifyAssertion from 'testdouble-qunit';
+import { setup } from 'qunit-dom';
+import { installWaitFor } from 'qunit-wait-for';
+
+installWaitFor(QUnit);
+installVerifyAssertion(QUnit, td);
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
